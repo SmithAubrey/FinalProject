4b283ef8de043f2b1fbf68f2dcf54d5e1746321a
SmithAubrey <Smith.a201132@stu.sanjac.edu>
2026-04-30 17:35:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-const slides = document.querySelectorAll('.slides img');
-const nextBtn = document.getElementById('nextBtn');
-const prevBtn = document.getElementById('prevBtn');
-let currentSlide = 0;
-
-function showSlide(index) {
-  slides[currentSlide].classList.remove('active');
-  currentSlide = (index + slides.length) % slides.length;
-  slides[currentSlide].classList.add('active');
-}
-
-nextBtn.addEventListener('click', () => showSlide(currentSlide + 1));
-prevBtn.addEventListener('click', () => showSlide(currentSlide - 1));
 
 
